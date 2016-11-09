b2479896d78f6f1fa41e3a4ab183a819a1aa2ce5
yoer <iamyoer@gmail.com>
2016-11-09 10:03:42 +0800

    add create sh script, to create git submodule after a new clone

---
+++ submodule-create.sh
@@ -0,0 +1,31 @@
+cd build
+git init
+git remote add origin https://github.com/qters/build
+
+cd ./../qrchaos
+git init
+git remote add origin https://github.com/qters/qrchaos
+
+cd ./../qrcommon
+git init
+git remote add origin https://github.com/qters/qrcommon
+
+cd ./../qrframe
+git init
+git remote add origin https://github.com/qters/qrframe
+
+cd ./../qrlogger
+git init
+git remote add origin https://github.com/qters/qrlogger
+
+cd ./../qrorm
+git init
+git remote add origin https://github.com/qters/qrorm
+
+cd ./../qrtest
+git init
+git remote add origin https://github.com/qters/qrtest
+
+cd ./../qrwidgets
+git init
+git remote add origin https://github.com/qters/qrwidgets
